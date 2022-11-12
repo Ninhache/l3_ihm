dc7c36fc9b7b24019fc65e26428acd528906ac22
Neo <neo.almeida.etu@univ-lille.fr>
2022-11-12 13:46:24 +0100

    fix: Step 2 !

--- tp1/mainWindow.py
+++ tp1/mainWindow.py
@@ -1,8 +1,19 @@
 import sys
+from PyQt5.QtWidgets import *
 
 def main(args):
-	print("execution de la fonction main")
+	app = QApplication(args)
+	window = QMainWindow()
+	window.show()
+	sys.exit(app.exec_())
 
 if __name__ == "__main__":
 	print("execution du programme")
 	main(sys.argv)
+
+class MainWindow(QMainWindow):
+	def __init__(self):
+		super().__init__()
+		self.setWindowTitle("tp1")
+		self.resize(800, 600)
+		self.show()
