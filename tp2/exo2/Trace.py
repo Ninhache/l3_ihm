a814d30e8597f3065e8a3d038ffc5e8ca69979ec
Neo <neo.almeida.etu@univ-lille.fr>
2022-11-29 00:27:50 +0100

    2nd step

---
+++ tp2/exo2/Trace.py
@@ -0,0 +1,10 @@
+class Trace():
+    def __init__(self, width, color):
+        self.points = []
+        self.width = width
+        self.color = color
+
+    def addPoint(self, point):
+        self.points.append(point)
+
+    
