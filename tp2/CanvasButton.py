0f50b7532e438ad2f8aa2a208b08b0a0ad765adf
Neo <neo.almeida.etu@univ-lille.fr>
2022-11-28 23:35:54 +0100

    Exo 1 fini

--- tp2/CanvasButton.py
+++ tp2/CanvasButton.py
@@ -2,49 +2,73 @@ from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 from PyQt5.QtCore import *
 
+import ButtonModel
+
 class CanvasButton(QWidget):
 	def __init__(self, parent):
 		super().__init__(parent)
 		self.parent = parent
 		self.createButton()
-		self.cursorOver = False
+		#self.cursorOver = False
+		# draw elipse
+		self.bbox = QRect(0, 0, 100, 100)
+		self.defaultColor = QColor(100,100,100)
 
-		self.bbox = QRect(10,10,100,100)
-		self.defaultColor = QColor(0,0,0)
+		# hoverCol et pressCol 
+		self.hoverCol = QColor(65, 166, 255)
+		self.pressCol = QColor(71, 65, 255)
+
+		self.setMouseTracking(True)
 
 		self.mouseMoveEvent = self.mouseMoveEvent
 		self.mousePressEvent = self.mousePressEvent
 		self.mouseReleaseEvent = self.mouseReleaseEvent
 
+
+		self.model = ButtonModel.ButtonModel(self)
+
 	def createButton(self):
 		self.setFixedSize(self.parent.width(), self.parent.height())
 
 	def mouseMoveEvent(self, event):
-		self.cursorOver = self.cursorOnEllipse(event.pos())
-		if self.bbox.contains(event.pos()):
-			self.defaultColor = QColor(255,0,0)
+		if (self.cursorOnEllipse(event.pos())):
+			self.model.enterHover()
 		else:
-			self.defaultColor = QColor(0,0,0)
+			self.model.leaveHover()
 		self.repaint()
 
 	def mousePressEvent(self, event):
-		if self.bbox.contains(event.pos()):
-			self.defaultColor = QColor(0,255,0)
-		else:
-			self.defaultColor = QColor(0,0,0)
-		self.repaint()
+		if (self.cursorOnEllipse(event.pos())):
+			self.model.entrePress()
+			self.repaint()
 
 	def mouseReleaseEvent(self, event):
-		if self.bbox.contains(event.pos()):
-			self.defaultColor = QColor(0,0,255)
-		else:
-			self.defaultColor = QColor(0,0,0)
+		self.model.leavePress()
 		self.repaint()
 
 	def paintEvent(self, event):
 		painter = QPainter(self)
-		painter.setPen(self.defaultColor)
-		painter.drawRect(self.bbox)
+		painter.setRenderHint(QPainter.Antialiasing)
+		pen = QPen()
+		pen.setWidth(2)
+		pen.setColor(QColor(0,0,0))
+		painter.setPen(pen)
+		
+		if self.model.state == ButtonModel.State.IDLE:
+			painter.setBrush(self.defaultColor)
+		elif self.model.state == ButtonModel.State.HOVER:
+			painter.setBrush(self.hoverCol)
+		elif self.model.state == ButtonModel.State.PRESSIN:
+			painter.setBrush(self.pressCol)
+		elif self.model.state == ButtonModel.State.PRESSOUT:
+			painter.setBrush(self.defaultColor)
+
+		painter.drawEllipse(self.bbox)
+
+
+
+		
+
 
 	def cursorOnEllipse(self, point):
 		return self.bbox.contains(point)
